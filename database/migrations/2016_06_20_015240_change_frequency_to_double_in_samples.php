e6ddfd2f2d72a4ebcaef2a4de183388a04fee571
Kevin Peña <kevin.pena.prog@gmail.com>
2016-06-20 01:57:00 +0000

    Add composer dependency: doctrine/dbal

---
+++ database/migrations/2016_06_20_015240_change_frequency_to_double_in_samples.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeFrequencyToDoubleInSamples extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('samples', function (Blueprint $table) {
+            $table->double('frequency')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('samples', function (Blueprint $table) {
+            $table->integer('frequency')->change();
+        });
+    }
+}
