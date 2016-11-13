d7d597da1e02ce31ae119c92818bc9f6d99872e2
Kevin Peña <kevin.pena.prog@gmail.com>
2016-11-13 18:57:06 -0500

    Remove map_campos link for als

--- resources/views/partial/sla_links.blade.php
+++ resources/views/partial/sla_links.blade.php
@@ -1,3 +1,3 @@
 <li><a href="/sla/map/pozos">Mapa por Pozos</a></li>
-<li><a href="/sla/map/campos">Mapa por Campos</a></li>
+{{-- <li><a href="/sla/map/campos">Mapa por Campos</a></li> --}}
 <li><a href="/sla/matrix">Matriz de Seleccion Preliminar</a></li>
